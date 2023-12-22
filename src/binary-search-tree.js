134d624841edd9dad43466918df9d840f62ca8c4
Oxana Savianok <oxanas8833@gmail.com>
2023-12-22 21:16:26 +0300

    feat: add the beginning of solving the binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,18 +7,52 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.rootTree = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
+    return this.rootTree;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    this.rootTree = addWithin(this.rootTree, data);
+    function addWithin(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+      return node;
+    }
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return searchWithin(this.rootTree, data);
+    function searchWithin(node, data) {
+      if (!node) {
+        return false;
+      }
+      if (node.data === data) {
+        return true;
+      }
+      if  (data < node.data) {
+        return searchWithin(node.left, data);
+      } else {
+        return searchWithin(node.right, data);
+      }
+    }
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -14,13 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined using interface*/
-class ListNode {
-  constructor(x) {
-    this.value = x;
-    this.next = null;
-  }
-}
+ * Singly - linked lists are already defined using interface
+ * class ListNode {
+ *   constructor(x) {
+ *    this.value = x;
+ *    this.next = null;
+ *   }
+ * }
+ */
  
 function removeKFromList(l, k) {
   let elem = l;
